5f10ed6a6b9294d5e6405d1fe869c336f34ca7a1
SIYEA J <siyea0405@gmail.com>
2023-03-16 17:28:42 +0900

    Add: Add Builds Data and change single quote

---
+++ src/utils/buildsData.ts
@@ -0,0 +1,117 @@
+export const buildData = [
+  {
+    number: 1,
+    build_name: '감경돌소교복',
+    specialization: '기술전문가',
+    weapons: {
+      main_weapon: '왕분쇄자',
+      sub_weapon:
+        'SIX12/M1A CQB(심정지), 카빈7/경찰M4(빠른손 or 심정지) - 비엄댐',
+      Pistol: 'TDI 카드',
+    },
+    protecs: {
+      protec_1: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+      },
+      protec_2: {
+        brand_name: '펜리르 그룹 AB',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+        talent: '감시병',
+      },
+      protec_3: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+      protec_4: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+        telent: '경계',
+      },
+      protec_5: {
+        brand_name: '그루포 솜브라',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+      protec_6: {
+        brand_name: '오버로드 군수산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+    },
+    skills: {
+      skill_1: '성전사 방패',
+      skill_2: '소생 하이브',
+    },
+  },
+  {
+    number: 2,
+    build_name: '소총교복=완집딱딱이',
+    specialization: '명사수',
+    weapons: {
+      main_weapon: '클래식M1A',
+      sub_weapon: '돌격소총',
+      Pistol: 'TDI 카드',
+    },
+    protecs: {
+      protec_1: {
+        brand_name: '그루포 솜브라',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+      },
+      protec_2: {
+        brand_name: '아이랄디 홀딩',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+        talent: '훌륭한 귀감',
+      },
+      protec_3: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+      protec_4: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+        module: '치명타 데미지',
+        telent: '경계',
+      },
+      protec_5: {
+        brand_name: '프로비던스 방위산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+      protec_6: {
+        brand_name: '오버로드 군수산업',
+        main: '공격',
+        typeA: '치명타 확률',
+        typeB: '치명타 데미지',
+      },
+    },
+    skills: {
+      skill_1: '성전사 방패',
+      skill_2: '소생 하이브',
+    },
+  },
+];
